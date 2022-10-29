7962d74c5508be0b4dfeba0f97a123d6626a2e84
Zaykka <zay082008@gmail.com>
2022-10-29 15:01:03 +0300

    add User model to admin and customize them

--- backend/messenger/users/admin.py
+++ backend/messenger/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from users.models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name')
+    list_filter = ('username',)
+    row_id_fields = ('username',)
+    list_editable = ('bio',)
+    search_fields = ('username', 'first_name', 'last_name')
+
+
+admin.site.register(User)
+
--- backend/messenger/users/models.py
+++ backend/messenger/users/models.py
@@ -1,9 +1,19 @@
-from distutils.command.upload import upload
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-class User(models.Model):
-    user_name = models.CharField(max_length=256)
-    phone_number = models.CharField(max_length=100)
-    profile_photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    chat_id = models.ManyToManyField('chats.Chat')
+class User(AbstractUser):
+    username = models.CharField(unique=True, max_length=256, verbose_name='ник')
+    first_name = models.CharField(max_length=256, null=True, verbose_name='Имя')
+    last_name = models.CharField(max_length=256, null=True, verbose_name='Фамилия')
+    phone_number = models.CharField(max_length=100, verbose_name='номер телефона')
+    bio = models.TextField(null=True, verbose_name='описание профиля')
+    is_online = models.BooleanField(default=False, verbose_name='онлайн')
+    profile_photo = models.ImageField(upload_to='photos/%Y/%m/%d/', null=True, verbose_name='фото профиля')
+
+    def __str__(self):
+            return self.username
+    
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
