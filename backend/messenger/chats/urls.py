52fa30af11ad8795e7a146dd5493bc765f4cf5fb
Zaykka <zay082008@gmail.com>
2022-10-29 15:00:14 +0300

    add view delete chat, edit chat and create chat + rewrite other views so that real objects are created

--- backend/messenger/chats/urls.py
+++ backend/messenger/chats/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from chats.views import chat_list, create_chat, chat_detail
+from chats.views import *
 
 
 urlpatterns = [
-    path('', chat_list, name='chat_list'),
+    path('users/<int:user_pk>', chat_list, name='chat_list'),
     path('<chat_id>/', chat_detail, name='chat_detail'),
     path('create/<int:pk>/', create_chat, name='create_chat'), 
+    path('edit/<int:pk>/', edit_chat, name='edit_chat'), 
+    path('delete/<int:pk>/', delete_chat, name='delete_chat'), 
 ]
--- backend/messenger/chats/views.py
+++ backend/messenger/chats/views.py
@@ -1,21 +1,89 @@
+import re
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
+from chats.models import Chat, Message
+from users.models import User
 
 
 @require_http_methods(["GET"])
-def chat_list(request):
+def chat_list(request, user_pk):
     """Список чатов"""
-    return JsonResponse({'chats': []})
+    chats = []
+    user = get_object_or_404(User, id=user_pk)
+    for chat in Chat.objects.filter(users=user):
+        chats.append({'id': chat.id,
+                      'name': chat.name,
+                      'kind': chat.kind,
+                      'photo': str(chat.photo),
+                      'author': str(chat.author)})
+    return JsonResponse({'chats': chats})
+
 
 @require_http_methods(["GET"])
 def chat_detail(request, chat_id):
     """Страница чата"""
-    return JsonResponse({'chats': chat_id})
+    messages = []
+    chat = get_object_or_404(Chat, id=chat_id)
+    for message_meta in Message.objects.filter(chat=chat):
+        messages.append(
+            {'id': message_meta.id,
+            'sent_from': str(message_meta.sent_from),
+            'message': message_meta.message,
+            'created_at': message_meta.created_at,
+            'photo': str(message_meta.photo)})
+    return JsonResponse({'chats': messages})
 
-@csrf_exempt
+
 @require_http_methods(["POST"])
 def create_chat(request, pk):
-    """Создание чата"""
-    return JsonResponse({'chat_pk': pk})
+    name = request.POST.get("name")
+    kind = request.POST.get('kind')
+    photo = request.POST.get('photo')
+    author = int(request.POST.get('author'))
+    users = request.POST.get('users').split()
+    author_model = get_object_or_404(User, id=author)
+    new_chat = Chat.objects.create(name=name, kind=kind, photo=photo, author=author_model)
+    new_chat.save()
+    for user in users:
+        user_model = get_object_or_404(User, id=user)
+        new_chat.users.add(user_model)
+    return JsonResponse({'created': True})
+
+
+@require_http_methods(["POST"])
+def edit_chat(request, pk):
+    chat_model = get_object_or_404(Chat, id=pk)
+    name = request.POST.get("name")
+    kind = request.POST.get('kind')
+    photo = request.POST.get('photo')
+    author = request.POST.get('author')
+    users = request.POST.get('users')
+    
+    if name is not None:
+        chat_model.name = name
+    if kind is not None:
+        chat_model.name = kind
+    if photo is not None:
+        chat_model.name = photo
+    if author is not None:
+        author_model = get_object_or_404(User, id=int(author))
+        chat_model.author = author_model
+    if users is not None:
+        for user in users.split():
+            user_model = get_object_or_404(User, id=user)
+            chat_model.users.add(user_model)
+    
+    chat_model.save()
+    return JsonResponse({'edited': True})
+
+@require_http_methods(["POST"])
+def delete_chat(request, pk):
+    chat_model = get_object_or_404(Chat, id=pk)
+    if chat_model:
+        chat_model.delete()
+        return JsonResponse({'deleted': True})
+    return JsonResponse({'deleted': False})
+
+
