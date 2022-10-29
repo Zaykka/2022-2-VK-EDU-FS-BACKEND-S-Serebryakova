ed17e8151998481082e4bdd6001094429293d677
Zaykka <zay082008@gmail.com>
2022-10-29 14:55:49 +0300

    add model Chat and Message to admin and customize them

--- backend/messenger/chats/admin.py
+++ backend/messenger/chats/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from chats.models import Chat, Message
 
-# Register your models here.
+
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ('name', 'kind', 'author')
+    list_filter = ('author',)
+    row_id_fields = ('author',)
+    list_editable = ('kind',)
+    search_fields = ('name', 'author')
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('chat', 'sent_from', 'message', 'created_at')
+    list_filter = ('chat', 'sent_from',)
+    row_id_fields = ('sent_from',)
+    search_fields = ('chat', 'sent_from',)
+
+
+admin.site.register(Chat, ChatAdmin)
+admin.site.register(Message, MessageAdmin)
